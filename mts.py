8cb9e4884c6da4ec7b3ccd4cb9d2e5c68e130d52
Dave Lugg <acidictadpole@gmail.com>
2018-07-09 16:14:52 -0400

    Can run on multiple passed in missions now. Should work with shell expansion (i.e. `mts.py *.miz`)

--- mts.py
+++ mts.py
@@ -14,6 +14,7 @@ times = {
 
 
 def change_mission_time(misFile, time):
+    rgx = re.compile("^\s{4}\[\"start_time")
     with fileinput.input(misFile, inplace=True) as fp:
         for line in fp:
             if rgx.match(line):
@@ -21,47 +22,52 @@ def change_mission_time(misFile, time):
             sys.stdout.write(line)
 
 
-fn = sys.argv[1]
-if os.path.exists(fn):
-    path = os.path.abspath(fn)
-    print("path: {}".format(path))
-    basedir = os.path.dirname(path)
-    print("basedir: {}".format(basedir))
-    targetdir = "{}/.tmp".format(basedir)
-    print("targetdir: {}".format(targetdir))
-    print("Making tmp dir: {}".format(targetdir))
-    if os.path.exists(targetdir):
+
+def handle_mission(fn):
+    if os.path.exists(fn):
+        path = os.path.abspath(fn)
+        print("path: {}".format(path))
+        basedir = os.path.dirname(path)
+        print("basedir: {}".format(basedir))
+        targetdir = "{}/.tmp".format(basedir)
+        print("targetdir: {}".format(targetdir))
+        print("Making tmp dir: {}".format(targetdir))
+        if os.path.exists(targetdir):
+            shutil.rmtree(targetdir)
+        os.makedirs(targetdir)
+
+        print("Extracting zip: {}".format(fn))
+        zip_ref = zipfile.ZipFile(fn, 'r')
+        zip_ref.extractall(targetdir)
+
+        misfile = "{}/mission".format(targetdir)
+
+        new_files = []
+        for descr, time in times.items():
+            change_mission_time(misfile, str(time))
+            new_file = "{}/{}-{}".format(
+                basedir,
+                fn[:-4],
+                descr
+            )
+            shutil.make_archive(new_file, 'zip', targetdir)
+            new_files.append(new_file)
+
+        new_dir = "{}/{}".format(basedir, fn)[:-4]
+        print("New dir: " + new_dir)
+        if os.path.exists(new_dir) and os.path.isdir(new_dir):
+            shutil.rmtree(new_dir)
+        os.makedirs(new_dir)
+
+        for new_file in new_files:
+            filename = new_file+".zip"
+            print("new_file: " + new_file)
+            shutil.move(filename, new_dir+"/"+os.path.basename(new_file)+".miz")
+
+        #Clean up tmp dir.
         shutil.rmtree(targetdir)
-    os.makedirs(targetdir)
 
-    print("Extracting zip: {}".format(fn))
-    zip_ref = zipfile.ZipFile(fn, 'r')
-    zip_ref.extractall(targetdir)
+files = sys.argv[1:]
 
-    rgx = re.compile("^\s{4}\[\"start_time")
-    misfile = "{}/mission".format(targetdir)
-
-    new_files = []
-    for descr, time in times.items():
-        change_mission_time(misfile, str(time))
-        new_file = "{}/{}-{}".format(
-            basedir,
-            fn[:-4],
-            descr
-        )
-        shutil.make_archive(new_file, 'zip', targetdir)
-        new_files.append(new_file)
-
-    new_dir = "{}/{}".format(basedir, fn)[:-4]
-    print("New dir: " + new_dir)
-    if os.path.exists(new_dir) and os.path.isdir(new_dir):
-        shutil.rmtree(new_dir)
-    os.makedirs(new_dir)
-
-    for new_file in new_files:
-        filename = new_file+".zip"
-        print("new_file: " + new_file)
-        shutil.move(filename, new_dir+"/"+os.path.basename(new_file)+".miz")
-
-    #Clean up tmp dir.
-    shutil.rmtree(targetdir)
+for filename in files:
+    handle_mission(filename)
